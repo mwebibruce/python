1ac67867db8511a1a8ffb59f8d0d6acae7b7a107
mwebibruce <brucemwebi@outlook.com>
2025-04-29 12:07:44 +0300

    Added square function

--- myfile.py
+++ myfile.py
@@ -5,13 +5,22 @@ names=input("Enter names...\n")
 print("wecome "+names)
 # Add numbers
 def add(a,b):
-    return a+b;
+    return a+b
 
 ans=add(20,1)
 print(" result "+str(ans))
 # Multiply numbers
-def multipy(x,y):
-    return x*y
-result=multipy(x,y)
+def multiply(q,r):
+    return q*r
+result=multiply(9,0)
 print(result)
+
+# square
+def square(h):
+    return h*h
+
+#test square
+result=square(11)
+print(" square is "+str(result))
+
 
