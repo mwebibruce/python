9f585544ac0cb04161af95d89fa63cf09e143130
mwebibruce <brucemwebi@outlook.com>
2025-04-29 11:50:11 +0300

    the test

--- myfile.py
+++ myfile.py
@@ -1,2 +1,11 @@
 #mwebibranch content
 #more details
+print(" welome to python test program.")
+names=input("Enter names...\n")
+print("wecome "+names)
+# Add numbers
+def add(a,b):
+    return a+b;
+
+ans=add(20,1)
+print(" result "+str(ans))
