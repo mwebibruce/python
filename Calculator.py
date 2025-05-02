7f651c6744e82b7f902f0c53714fc5cb8c808b3b
Onyash Ed <onyashed@gmail.com>
2025-05-02 12:10:02 +0300

    Added Looper for cols

--- Calculator.py
+++ Calculator.py
@@ -1,6 +1,8 @@
  
+#import pandas as pd
 class Calculator:
   x = 5
+  
   def perimeter (self,l,w):
     return 2*(l+w)
   
@@ -15,19 +17,25 @@ class Calculator:
 
 
 # square
-def square(h):
-     return h*h
+  def square(self,h):
+    return h*h
+  def fileopener(self,filename ):
+    f=open (filename)
+    print (f.read())
+    
+ 
+  def pandasfileapp(self, filename):
+    pass  
 
-def fileopener(self,filename ):
-   f=open (filename)
-   print (f.read())
-
-
-
-
+    
+    #pd.read_csv(filename)
+    #print(pd.DataFrame[0])
 
 calc1=Calculator()
 print(calc1.trianglearea(4,4))
 print(calc1.perimeter(10,21))
+print(calc1.square(10))
+
 
 calc1.fileopener('results.csv')
+calc1.pandasfileapp('results.csv')
