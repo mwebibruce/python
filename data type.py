ac914b7436a94e1de0488d4eb109352e97ada016
mwebibruce <brucemwebi@outlook.com>
2025-05-06 10:54:05 +0300

    worked on arrays data type files

---
+++ data type.py
@@ -0,0 +1,12 @@
+cars=["Ford", "Volvo", "BMW"]
+#cars[0]="marcedes"
+x = cars[0]
+#print(cars[0])
+cars.pop(2)
+#print(cars[0])
+cars.remove("Volvo")
+y=len(cars)
+print (y)
+for x in cars:
+    print (x)
+
